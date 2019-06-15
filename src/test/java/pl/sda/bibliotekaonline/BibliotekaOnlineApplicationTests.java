da61a36d6b733c2edb6d0189da3c6704b9b091a4
Monika halicka-Miks <kontakt.miks@wp.pl>
2019-06-15 10:06:16 +0200

    initial commit

---
+++ src/test/java/pl/sda/bibliotekaonline/BibliotekaOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.bibliotekaonline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BibliotekaOnlineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
