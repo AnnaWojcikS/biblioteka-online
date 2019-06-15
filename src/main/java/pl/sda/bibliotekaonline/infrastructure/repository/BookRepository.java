ccf84a98702f76fe1333b060af710756aa285f21
Anna Wojcik <aaswieboda@gmail.com>
2019-06-15 12:57:44 +0200

    adding repository

---
+++ src/main/java/pl/sda/bibliotekaonline/infrastructure/repository/BookRepository.java
@@ -0,0 +1,18 @@
+package pl.sda.bibliotekaonline.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.bibliotekaonline.infrastructure.entity.Book;
+
+import java.util.List;
+
+/**
+ * Created by A.Wójcik on 15 06 2019.
+ */
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByCategory(String searchedCategory);
+
+
+}
